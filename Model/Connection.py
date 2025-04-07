ff2a8b0c7c5bbebb062be76e6757aa4ef1e3b1bb
erick-bonifacio <erickalvarengabonifacio@hotmail.com>
2025-04-07 15:00:55 -0300

    first commit

---
+++ Model/Connection.py
@@ -0,0 +1,30 @@
+from psycopg import connect, OperationalError
+from dotenv import load_dotenv
+import os
+
+class Connection:
+    def __init__(self):
+        load_dotenv()  
+
+        self.host = os.getenv("DB_HOST")
+        self.dbname = os.getenv("DB_NAME")
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASSWORD")
+        self.conn = None
+
+    def connect(self):
+        try:
+            self.conn = connect(
+                host=self.host,
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password
+            )
+            return self.conn
+        except OperationalError as e:
+            return None
+
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+            print("🔌 Conexão encerrada.")
---
+++ README.md
@@ -0,0 +1,12 @@
+# Projeto de Conexão com PostgreSQL
+
+Este projeto faz uma conexão com banco PostgreSQL usando Python, `psycopg` e variáveis de ambiente.
+
+## Como rodar
+
+1. Clone o projeto
+2. Crie um `.env` com os dados do seu banco
+3. Instale as dependências:
+
+```bash
+pip install -r requirements.txt
